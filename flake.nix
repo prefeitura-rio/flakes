adfb8efb3cd4f75289fee50b2fd7d78600a25fb4
Pedro Mendes <pedrohrmendes@proton.me>
2026-07-23 15:26:44 -0300

    feat: add git-hooks input and expose lib.mkPreCommitCheck

--- flake.nix
+++ flake.nix
@@ -4,41 +4,64 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    git-hooks.url = "github:cachix/git-hooks.nix";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
     inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import inputs.nixpkgs { inherit system; };
-        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "flake.nix";
-          programs = {
-            ruff-format.enable = true;
-            ruff-check.enable = true;
-            nixfmt.enable = true;
-            shfmt.enable = true;
-            shellcheck.enable = true;
+    let
+      perSystem = inputs.flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+          treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+            projectRootFile = "flake.nix";
+            programs = {
+              ruff-format.enable = true;
+              ruff-check.enable = true;
+              nixfmt.enable = true;
+              shfmt.enable = true;
+              shellcheck.enable = true;
+            };
           };
-        };
-      in
-      {
-        packages = import ./packages.nix { inherit pkgs; };
-        formatter = treefmtEval.config.build.wrapper;
-        checks.formatting = treefmtEval.config.build.check;
+        in
+        {
+          packages = import ./packages.nix { inherit pkgs; };
+          formatter = treefmtEval.config.build.wrapper;
+          checks.formatting = treefmtEval.config.build.check;
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            basedpyright
-            treefmtEval.config.build.wrapper
-            (python3.withPackages (ps: [
-              ps.loguru
-              ps.typer
-            ]))
-          ];
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              basedpyright
+              treefmtEval.config.build.wrapper
+              (python3.withPackages (ps: [
+                ps.loguru
+                ps.typer
+              ]))
+            ];
+          };
+        }
+      );
+    in
+    perSystem
+    // {
+      lib.mkPreCommitCheck =
+        {
+          system,
+          src,
+          pkgs,
+          extraHooks ? { },
+        }:
+        inputs.git-hooks.lib.${system}.run {
+          inherit src;
+          hooks = {
+            gitleaks.enable = true;
+            terraform-fmt = {
+              enable = true;
+              package = pkgs.opentofu;
+            };
+          } // extraHooks;
         };
-      }
-    );
+    };
 }
