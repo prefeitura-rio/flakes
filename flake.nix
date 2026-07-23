7097cfa8a9dc3c8a46362c424b797ed347fbe72e
Pedro Mendes <pedrohrmendes@proton.me>
2026-07-23 12:11:51 -0300

    refactor: replace flake-parts with flake-utils eachDefaultSystem

--- flake.nix
+++ flake.nix
@@ -3,46 +3,42 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ inputs.treefmt-nix.flakeModule ];
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-
-      perSystem =
-        { pkgs, ... }:
-        {
-          packages = import ./packages.nix { inherit pkgs; };
-
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-            programs = {
-              ruff-format.enable = true;
-              ruff-check.enable = true;
-              nixfmt.enable = true;
-              shfmt.enable = true;
-              shellcheck.enable = true;
-            };
-          };
-
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              basedpyright
-              (python3.withPackages (ps: [
-                ps.loguru
-                ps.typer
-              ]))
-            ];
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        treefmtEval = inputs.treefmt-nix.lib.evalConfig pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            ruff-format.enable = true;
+            ruff-check.enable = true;
+            nixfmt.enable = true;
+            shfmt.enable = true;
+            shellcheck.enable = true;
           };
         };
-    };
+      in
+      {
+        packages = import ./packages.nix { inherit pkgs; };
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            basedpyright
+            treefmtEval.config.build.wrapper
+            (python3.withPackages (ps: [
+              ps.loguru
+              ps.typer
+            ]))
+          ];
+        };
+      }
+    );
 }
