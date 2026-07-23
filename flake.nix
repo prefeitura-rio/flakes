6d4a040b4e0d2d0d1585984f2def4713ab43cb2a
Pedro Mendes <pedrohrmendes@proton.me>
2026-07-23 15:30:48 -0300

    refactor: drop redundant outer let/in wrapper in outputs

--- flake.nix
+++ flake.nix
@@ -9,39 +9,36 @@
 
   outputs =
     inputs:
-    let
-      perSystem = inputs.flake-utils.lib.eachDefaultSystem (
-        system:
-        let
-          pkgs = import inputs.nixpkgs { inherit system; };
-          treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
-            projectRootFile = "flake.nix";
-            programs = {
-              ruff-format.enable = true;
-              ruff-check.enable = true;
-              nixfmt.enable = true;
-              shfmt.enable = true;
-              shellcheck.enable = true;
-            };
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            ruff-format.enable = true;
+            ruff-check.enable = true;
+            nixfmt.enable = true;
+            shfmt.enable = true;
+            shellcheck.enable = true;
           };
-        in
-        {
-          packages = import ./packages.nix { inherit pkgs; };
-          formatter = treefmtEval.config.build.wrapper;
-          checks.formatting = treefmtEval.config.build.check;
+        };
+      in
+      {
+        packages = import ./packages.nix { inherit pkgs; };
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check;
 
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              basedpyright
-              treefmtEval.config.build.wrapper
-              (python3.withPackages (ps: [
-                ps.loguru
-                ps.typer
-              ]))
-            ];
-          };
-        }
-      );
-    in
-    perSystem;
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            basedpyright
+            treefmtEval.config.build.wrapper
+            (python3.withPackages (ps: [
+              ps.loguru
+              ps.typer
+            ]))
+          ];
+        };
+      }
+    );
 }
